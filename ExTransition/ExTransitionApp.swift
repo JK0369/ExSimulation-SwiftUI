fbf56e3b26e04e9f114f91fcf78ff0510fcded9b
jake <palatable7@naver.com>
2022-10-06 10:37:15 +0900

    Initial Commit

---
+++ ExTransition/ExTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExTransitionApp.swift
+//  ExTransition
+//
+//  Created by 김종권 on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct ExTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
